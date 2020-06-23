3da389a1a599618b0e2989664cbe1258d2a31648
ivan.trabucco <ivan.trabucco@proximanetwork.it>
2020-06-23 09:55:54 +0200

    form WIP5...

--- my-project/src/app/registration-form/validations/validation-form.ts
+++ my-project/src/app/registration-form/validations/validation-form.ts
@@ -1,13 +1,15 @@
 import {AbstractControl} from '@angular/forms';
 
 export const mailValidator = (control: AbstractControl):  any => {
+  const val = control.value;
   const rex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,5})+$/);
-  return !rex.test(control.value) ? {'mailValidator': true} : null;
+  return val.length === 0 ? null : !rex.test(control.value) ? {'mailValidator': true} : null;
 };
 
 export const pswValidator = (control: AbstractControl): any => {
+  const val = control.value;
   const rex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,16}$/);
-  return !rex.test(control.value) ? {'pswValidator': true} : null;
+  return val.length === 0 ? null : !rex.test(val) ? {'pswValidator': true} : null;
 };
 
 
